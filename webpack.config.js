36c009a8a62c1dc040fcef28b433a3be3f7ade63
Marcin Brumer <brumek31@gmail.com>
2018-10-30 17:42:21 +0100

    Setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* === dont forget to import scss to main.js file === */
+/* ===> import './main.scss'; <=== */
+
+var path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/js/main.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  }
+};
